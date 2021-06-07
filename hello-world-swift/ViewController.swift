7833da80416de7beee32d83b79a8c3edd3c3c4f2
Vlad Tomsa <tomsavlad90@gmail.com>
2021-06-07 18:54:18 +0300

    Initial Commit

---
+++ hello-world-swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hello-world-swift
+//
+//  Created by Vlad Tomsa on 07.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
